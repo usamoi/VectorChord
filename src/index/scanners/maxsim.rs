5cd154e25948c767a5fa5d434a64847399e3493b
usamoi <usamoi@outlook.com>
2025-04-09 18:20:09 +0800

    fix: apply epsilon for default search
    Signed-off-by: usamoi <usamoi@outlook.com>

--- src/index/scanners/maxsim.rs
+++ src/index/scanners/maxsim.rs
@@ -88,7 +88,7 @@ impl SearchBuilder for MaxsimBuilder {
                     })
                     .collect::<Vec<_>>();
                 Box::new(vectors.into_iter().map(|vector| {
-                    let (results, estimation) = algorithm::maxsim_search::<Op>(
+                    let (results, estimation_by_threshold) = algorithm::maxsim_search::<Op>(
                         relation.clone(),
                         vector.clone(),
                         options.probes.clone(),
@@ -110,7 +110,7 @@ impl SearchBuilder for MaxsimBuilder {
                         let rough_iter = results.into_iter();
                         rough_set.extend(rough_iter.map(rough_map));
                     }
-                    (accu_set, rough_set, estimation)
+                    (accu_set, rough_set, estimation_by_threshold)
                 }))
             }
             VectorKind::Vecf16 => {
@@ -129,7 +129,7 @@ impl SearchBuilder for MaxsimBuilder {
                     })
                     .collect::<Vec<_>>();
                 Box::new(vectors.into_iter().map(|vector| {
-                    let (results, estimation) = algorithm::maxsim_search::<Op>(
+                    let (results, estimation_by_threshold) = algorithm::maxsim_search::<Op>(
                         relation.clone(),
                         vector.clone(),
                         options.probes.clone(),
@@ -151,44 +151,44 @@ impl SearchBuilder for MaxsimBuilder {
                         let rough_iter = results.into_iter();
                         rough_set.extend(rough_iter.map(rough_map));
                     }
-                    (accu_set, rough_set, estimation)
+                    (accu_set, rough_set, estimation_by_threshold)
                 }))
             }
         };
         let mut updates = Vec::new();
         let mut estimations = Vec::new();
-        for (query_id, (accu_set, rough_set, estimation)) in iter.enumerate() {
+        for (query_id, (accu_set, rough_set, estimation_by_threshold)) in iter.enumerate() {
             updates.reserve(accu_set.len() + rough_set.len());
             let is_empty = accu_set.is_empty() && rough_set.is_empty();
-            let mut estimation_by_accu = f32::NEG_INFINITY;
+            let mut estimation_by_scope = Distance::NEG_INFINITY;
             for (distance, payload) in accu_set {
-                estimation_by_accu = estimation_by_accu.max(distance.to_f32());
+                estimation_by_scope = std::cmp::max(estimation_by_scope, distance);
                 let (key, _) = pointer_to_kv(payload);
-                updates.push((key, AlwaysEqual((query_id, distance))));
+                updates.push((key, query_id, distance));
             }
             for (distance, payload) in rough_set {
                 let (key, _) = pointer_to_kv(payload);
-                updates.push((key, AlwaysEqual((query_id, distance))));
+                updates.push((key, query_id, distance));
             }
             estimations.push(if !is_empty {
-                estimation_by_accu.max(estimation.to_f32())
+                std::cmp::max(estimation_by_scope, estimation_by_threshold)
             } else {
-                0.0
+                Distance::ZERO
             });
         }
-        updates.sort_unstable();
+        updates.sort_unstable_by_key(|&(key, ..)| key);
         let iter = updates
-            .chunk_by(|(x, _), (y, _)| x == y)
+            .chunk_by(|(kl, ..), (kr, ..)| kl == kr)
             .map(|chunk| {
                 let key = chunk[0].0;
                 let mut value = vec![None; n];
-                for &(_, AlwaysEqual((query_id, distance))) in chunk {
+                for &(_, query_id, distance) in chunk {
                     let this = value[query_id].get_or_insert(Distance::INFINITY);
                     *this = std::cmp::min(*this, distance);
                 }
                 let mut maxsim = 0.0f32;
                 for (query_id, distance) in value.into_iter().enumerate() {
-                    let d = distance.unwrap_or(Distance::from_f32(estimations[query_id]));
+                    let d = distance.unwrap_or(estimations[query_id]);
                     maxsim += Distance::to_f32(d);
                 }
                 (Reverse(Distance::from_f32(maxsim)), AlwaysEqual(key))
