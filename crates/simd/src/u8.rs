8bb15be0710b9d83ad91fd9ad7a1409b29f3aebc
usamoi <usamoi@outlook.com>
2025-04-27 16:24:36 +0800

    feat: add vectorized version of u8::reduce_sum_of_xy
    Signed-off-by: usamoi <usamoi@outlook.com>

--- crates/simd/src/u8.rs
+++ crates/simd/src/u8.rs
@@ -1,5 +1,266 @@
 mod reduce_sum_of_xy {
-    #[crate::multiversion("v4", "v3", "v2", "a2")]
+    #[inline]
+    #[cfg(target_arch = "x86_64")]
+    #[crate::target_cpu(enable = "v4")]
+    fn reduce_sum_of_xy_v4(s: &[u8], t: &[u8]) -> u32 {
+        use core::arch::x86_64::*;
+        assert_eq!(s.len(), t.len());
+        let n = s.len();
+        let lo = _mm512_set1_epi16(0x00ff_u16 as i16);
+        let hi = _mm512_set1_epi16(0xff00_u16 as i16);
+        let mut _0 = _mm512_setzero_si512();
+        let mut _1 = _mm512_setzero_si512();
+        let mut _2 = _mm512_setzero_si512();
+        let mut _3 = _mm512_setzero_si512();
+        for i in (0..n).step_by(64) {
+            let mask = _bzhi_u64(u64::MAX, n as u32);
+            let s = unsafe { _mm512_maskz_loadu_epi8(mask, s.as_ptr().add(i).cast()) };
+            let t = unsafe { _mm512_maskz_loadu_epi8(mask, t.as_ptr().add(i).cast()) };
+            let s_l = _mm512_and_si512(s, lo);
+            let s_h = _mm512_and_si512(_mm512_srli_epi16(_mm512_and_si512(s, hi), 8), lo);
+            let t_l = _mm512_and_si512(t, lo);
+            let t_h = _mm512_and_si512(_mm512_srli_epi16(_mm512_and_si512(t, hi), 8), lo);
+            let l = _mm512_mullo_epi16(s_l, t_l);
+            let h = _mm512_mullo_epi16(s_h, t_h);
+            _0 = _mm512_add_epi32(_0, _mm512_cvtepu16_epi32(_mm512_extracti32x8_epi32(l, 0)));
+            _1 = _mm512_add_epi32(_1, _mm512_cvtepu16_epi32(_mm512_extracti32x8_epi32(l, 1)));
+            _2 = _mm512_add_epi32(_2, _mm512_cvtepu16_epi32(_mm512_extracti32x8_epi32(h, 0)));
+            _3 = _mm512_add_epi32(_3, _mm512_cvtepu16_epi32(_mm512_extracti32x8_epi32(h, 1)));
+        }
+        let _5 = _mm512_add_epi32(_0, _1);
+        let _6 = _mm512_add_epi32(_2, _3);
+        _mm512_reduce_add_epi32(_mm512_add_epi32(_5, _6)) as u32
+    }
+
+    #[cfg(all(target_arch = "x86_64", test, not(miri)))]
+    #[test]
+    fn reduce_sum_of_xy_v4_test() {
+        use rand::Rng;
+        if !crate::is_cpu_detected!("v4") {
+            println!("test {} ... skipped (v4)", module_path!());
+            return;
+        }
+        let mut rng = rand::rng();
+        for _ in 0..if cfg!(not(miri)) { 256 } else { 1 } {
+            let n = 4016;
+            let lhs = (0..n).map(|_| rng.random()).collect::<Vec<_>>();
+            let rhs = (0..n).map(|_| rng.random()).collect::<Vec<_>>();
+            for z in 3984..4016 {
+                let lhs = &lhs[..z];
+                let rhs = &rhs[..z];
+                let specialized = unsafe { reduce_sum_of_xy_v4(lhs, rhs) };
+                let fallback = fallback(lhs, rhs);
+                assert!(
+                    specialized == fallback,
+                    "specialized = {specialized}, fallback = {fallback}."
+                );
+            }
+        }
+    }
+
+    #[inline]
+    #[cfg(target_arch = "x86_64")]
+    #[crate::target_cpu(enable = "v3")]
+    fn reduce_sum_of_xy_v3(s: &[u8], t: &[u8]) -> u32 {
+        use crate::emulate::emulate_mm256_reduce_add_epi32;
+        use core::arch::x86_64::*;
+        assert_eq!(s.len(), t.len());
+        let n = s.len();
+        let lo = _mm256_set1_epi16(0x00ff_u16 as i16);
+        let hi = _mm256_set1_epi16(0xff00_u16 as i16);
+        let mut _0 = _mm256_setzero_si256();
+        let mut _1 = _mm256_setzero_si256();
+        let mut _2 = _mm256_setzero_si256();
+        let mut _3 = _mm256_setzero_si256();
+        let mut i = 0;
+        while i + 32 <= n {
+            let s = unsafe { _mm256_loadu_si256(s.as_ptr().add(i).cast()) };
+            let t = unsafe { _mm256_loadu_si256(t.as_ptr().add(i).cast()) };
+            let s_l = _mm256_and_si256(s, lo);
+            let s_h = _mm256_and_si256(_mm256_srli_epi16(_mm256_and_si256(s, hi), 8), lo);
+            let t_l = _mm256_and_si256(t, lo);
+            let t_h = _mm256_and_si256(_mm256_srli_epi16(_mm256_and_si256(t, hi), 8), lo);
+            let l = _mm256_mullo_epi16(s_l, t_l);
+            let h = _mm256_mullo_epi16(s_h, t_h);
+            _0 = _mm256_add_epi32(_0, _mm256_cvtepu16_epi32(_mm256_extracti128_si256(l, 0)));
+            _1 = _mm256_add_epi32(_1, _mm256_cvtepu16_epi32(_mm256_extracti128_si256(l, 1)));
+            _2 = _mm256_add_epi32(_2, _mm256_cvtepu16_epi32(_mm256_extracti128_si256(h, 0)));
+            _3 = _mm256_add_epi32(_3, _mm256_cvtepu16_epi32(_mm256_extracti128_si256(h, 1)));
+            i += 32;
+        }
+        let _5 = _mm256_add_epi32(_0, _1);
+        let _6 = _mm256_add_epi32(_2, _3);
+        let mut result = emulate_mm256_reduce_add_epi32(_mm256_add_epi32(_5, _6)) as u32;
+        if i < n {
+            while i < n {
+                result += s[i] as u32 * t[i] as u32;
+                i += 1;
+            }
+        }
+        result
+    }
+
+    #[cfg(all(target_arch = "x86_64", test))]
+    #[test]
+    fn reduce_sum_of_xy_v3_test() {
+        use rand::Rng;
+        if !crate::is_cpu_detected!("v3") {
+            println!("test {} ... skipped (v3)", module_path!());
+            return;
+        }
+        let mut rng = rand::rng();
+        for _ in 0..if cfg!(not(miri)) { 256 } else { 1 } {
+            let n = 4016;
+            let lhs = (0..n).map(|_| rng.random()).collect::<Vec<_>>();
+            let rhs = (0..n).map(|_| rng.random()).collect::<Vec<_>>();
+            for z in 3984..4016 {
+                let lhs = &lhs[..z];
+                let rhs = &rhs[..z];
+                let specialized = unsafe { reduce_sum_of_xy_v3(lhs, rhs) };
+                let fallback = fallback(lhs, rhs);
+                assert!(
+                    specialized == fallback,
+                    "specialized = {specialized}, fallback = {fallback}."
+                );
+            }
+        }
+    }
+
+    #[inline]
+    #[cfg(target_arch = "x86_64")]
+    #[crate::target_cpu(enable = "v2")]
+    fn reduce_sum_of_xy_v2(s: &[u8], t: &[u8]) -> u32 {
+        use crate::emulate::emulate_mm_reduce_add_epi32;
+        use core::arch::x86_64::*;
+        assert_eq!(s.len(), t.len());
+        let n = s.len();
+        let lo = _mm_set1_epi16(0x00ff_u16 as i16);
+        let hi = _mm_set1_epi16(0xff00_u16 as i16);
+        let mut _0 = _mm_setzero_si128();
+        let mut _1 = _mm_setzero_si128();
+        let mut _2 = _mm_setzero_si128();
+        let mut _3 = _mm_setzero_si128();
+        let mut i = 0;
+        while i + 16 <= n {
+            let s = unsafe { _mm_loadu_si128(s.as_ptr().add(i).cast()) };
+            let t = unsafe { _mm_loadu_si128(t.as_ptr().add(i).cast()) };
+            let s_l = _mm_and_si128(s, lo);
+            let s_h = _mm_and_si128(_mm_srli_epi16(_mm_and_si128(s, hi), 8), lo);
+            let t_l = _mm_and_si128(t, lo);
+            let t_h = _mm_and_si128(_mm_srli_epi16(_mm_and_si128(t, hi), 8), lo);
+            let l = _mm_mullo_epi16(s_l, t_l);
+            let h = _mm_mullo_epi16(s_h, t_h);
+            _0 = _mm_add_epi32(_0, _mm_cvtepu16_epi32(l));
+            _1 = _mm_add_epi32(_1, _mm_cvtepu16_epi32(_mm_unpackhi_epi64(l, l)));
+            _2 = _mm_add_epi32(_2, _mm_cvtepu16_epi32(h));
+            _3 = _mm_add_epi32(_3, _mm_cvtepu16_epi32(_mm_unpackhi_epi64(h, h)));
+            i += 16;
+        }
+        let _5 = _mm_add_epi32(_0, _1);
+        let _6 = _mm_add_epi32(_2, _3);
+        let mut result = emulate_mm_reduce_add_epi32(_mm_add_epi32(_5, _6)) as u32;
+        if i < n {
+            while i < n {
+                result += s[i] as u32 * t[i] as u32;
+                i += 1;
+            }
+        }
+        result
+    }
+
+    #[cfg(all(target_arch = "x86_64", test))]
+    #[test]
+    fn reduce_sum_of_xy_v2_test() {
+        use rand::Rng;
+        if !crate::is_cpu_detected!("v2") {
+            println!("test {} ... skipped (v2)", module_path!());
+            return;
+        }
+        let mut rng = rand::rng();
+        for _ in 0..if cfg!(not(miri)) { 256 } else { 1 } {
+            let n = 4016;
+            let lhs = (0..n).map(|_| rng.random()).collect::<Vec<_>>();
+            let rhs = (0..n).map(|_| rng.random()).collect::<Vec<_>>();
+            for z in 3984..4016 {
+                let lhs = &lhs[..z];
+                let rhs = &rhs[..z];
+                let specialized = unsafe { reduce_sum_of_xy_v2(lhs, rhs) };
+                let fallback = fallback(lhs, rhs);
+                assert!(
+                    specialized == fallback,
+                    "specialized = {specialized}, fallback = {fallback}."
+                );
+            }
+        }
+    }
+
+    #[inline]
+    #[cfg(target_arch = "aarch64")]
+    #[crate::target_cpu(enable = "a2")]
+    fn reduce_sum_of_xy_a2(s: &[u8], t: &[u8]) -> u32 {
+        use core::arch::aarch64::*;
+        assert_eq!(s.len(), t.len());
+        let n = s.len();
+        let lo = vdupq_n_u16(0x00ff_u16);
+        let mut _0 = vdupq_n_u32(0);
+        let mut _1 = vdupq_n_u32(0);
+        let mut _2 = vdupq_n_u32(0);
+        let mut _3 = vdupq_n_u32(0);
+        let mut i = 0;
+        while i + 16 <= n {
+            let s = vreinterpretq_u16_u8(unsafe { vld1q_u8(s.as_ptr().add(i).cast()) });
+            let t = vreinterpretq_u16_u8(unsafe { vld1q_u8(t.as_ptr().add(i).cast()) });
+            let s_l = vandq_u16(s, lo);
+            let s_h = vshrq_n_u16(s, 8);
+            let t_l = vandq_u16(t, lo);
+            let t_h = vshrq_n_u16(t, 8);
+            let l = vmulq_u16(s_l, t_l);
+            let h = vmulq_u16(s_h, t_h);
+            _0 = vaddq_u32(_0, vmovl_u16(vget_low_u16(l)));
+            _1 = vaddq_u32(_1, vmovl_u16(vget_high_u16(l)));
+            _2 = vaddq_u32(_2, vmovl_u16(vget_low_u16(h)));
+            _3 = vaddq_u32(_3, vmovl_u16(vget_high_u16(h)));
+            i += 16;
+        }
+        let _5 = vaddq_u32(_0, _1);
+        let _6 = vaddq_u32(_2, _3);
+        let mut result = vaddvq_u32(vaddq_u32(_5, _6));
+        if i < n {
+            while i < n {
+                result += s[i] as u32 * t[i] as u32;
+                i += 1;
+            }
+        }
+        result
+    }
+
+    #[cfg(all(target_arch = "aarch64", test, not(miri)))]
+    #[test]
+    fn reduce_sum_of_xy_a2_test() {
+        use rand::Rng;
+        if !crate::is_cpu_detected!("a2") {
+            println!("test {} ... skipped (a2)", module_path!());
+            return;
+        }
+        let mut rng = rand::rng();
+        for _ in 0..if cfg!(not(miri)) { 256 } else { 1 } {
+            let n = 4016;
+            let lhs = (0..n).map(|_| rng.random()).collect::<Vec<_>>();
+            let rhs = (0..n).map(|_| rng.random()).collect::<Vec<_>>();
+            for z in 3984..4016 {
+                let lhs = &lhs[..z];
+                let rhs = &rhs[..z];
+                let specialized = unsafe { reduce_sum_of_xy_a2(lhs, rhs) };
+                let fallback = fallback(lhs, rhs);
+                assert!(
+                    specialized == fallback,
+                    "specialized = {specialized}, fallback = {fallback}."
+                );
+            }
+        }
+    }
+
+    #[crate::multiversion(@"v4", @"v3", @"v2", @"a2")]
     pub fn reduce_sum_of_xy(s: &[u8], t: &[u8]) -> u32 {
         assert_eq!(s.len(), t.len());
         let n = s.len();
