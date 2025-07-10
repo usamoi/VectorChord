d8ada16b4a725bbc8f149df0a9bb676f7fa57e55
usamoi <usamoi@outlook.com>
2025-07-10 16:30:16 +0800

    fix: emscripten
    Signed-off-by: usamoi <usamoi@outlook.com>

--- crates/simd/build.rs
+++ crates/simd/build.rs
@@ -14,19 +14,51 @@
 
 use std::env::var;
 use std::error::Error;
+use std::ffi::OsString;
+
+fn compiler(host: &str, target: &str) -> Option<OsString> {
+    if let Some(cc) = std::env::var_os("CC") {
+        return Some(cc);
+    }
+    if let Some(cc) = std::env::var_os(format!("CC_{target}")) {
+        return Some(cc);
+    }
+    if let Some(cc) = std::env::var_os(format!("CC_{}", target.replace("-", "_"))) {
+        return Some(cc);
+    }
+    if host == target {
+        if let Ok(cc) = which::which("clang") {
+            return Some(cc.into());
+        }
+        for i in (16..=99).rev() {
+            if let Ok(cc) = which::which(format!("clang-{i}")) {
+                return Some(cc.into());
+            }
+        }
+    }
+    None
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("cargo::rerun-if-changed=cshim");
+    let host = var("HOST")?;
+    let target = var("TARGET")?;
     let target_arch = var("CARGO_CFG_TARGET_ARCH")?;
     match target_arch.as_str() {
         "aarch64" => {
             let mut build = cc::Build::new();
+            if let Some(compiler) = compiler(&host, &target) {
+                build.compiler(compiler);
+            }
             build.file("./cshim/aarch64.c");
             build.opt_level(3);
             build.compile("simd_cshim");
         }
         "x86_64" => {
             let mut build = cc::Build::new();
+            if let Some(compiler) = compiler(&host, &target) {
+                build.compiler(compiler);
+            }
             build.file("./cshim/x86_64.c");
             build.opt_level(3);
             build.compile("simd_cshim");
